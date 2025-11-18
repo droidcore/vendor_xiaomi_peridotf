34c96bda8ab5a56837d6cd12de633b2193e16fa8
zenin1504 <kayozzprojects@yandex.com>
2025-11-18 05:17:26 +0000

    peridot: Sync tree
    Change-Id: I0253930e6dd64da5a46cee337ad8c1f4
    Signed-off-by: zenin1504 <kayozzprojects@yandex.com>

--- Android.mk
+++ Android.mk
@@ -3,3 +3,31 @@
 #
 
 LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),peridot)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,f3895bf446cfd0e3ecb6fef3cc877208a05e87e3)
+$(call add-radio-file-sha1-checked,radio/aop.img,f7d64bdd675b7c3ca54fc5062a4318a61ba66a99)
+$(call add-radio-file-sha1-checked,radio/aop_config.img,38bb961dafdb060f9ceb00248b52ba7a8a603ed0)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,10625b73f1561ef66a51ce44028b3e1b727e6f27)
+$(call add-radio-file-sha1-checked,radio/cpucp.img,9a1dceced09f065e6344b02a45b6872d3c2df691)
+$(call add-radio-file-sha1-checked,radio/cpucp_dtb.img,9ef95062901acbaa9316061b021537cdce79fb66)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,27a28ab4c4f829cd8e3cfe5cc383ea55886fd4c9)
+$(call add-radio-file-sha1-checked,radio/dsp.img,44a5f33608169ead5b913bdac79492a6f70f23b8)
+$(call add-radio-file-sha1-checked,radio/featenabler.img,77cff6921a368900580322a7e4ed567074a00b02)
+$(call add-radio-file-sha1-checked,radio/hyp.img,7de9653c755e71be6847ea3cf05b975f9c2cd8da)
+$(call add-radio-file-sha1-checked,radio/imagefv.img,eff6b026e57cba948f8c3f792feb7e0a4376b4ad)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,80ca100dff6a55470fd9e8b18d300aec0d6fa976)
+$(call add-radio-file-sha1-checked,radio/modem.img,e0c44d48ed6009a6ffd477d6c1f014186255b451)
+$(call add-radio-file-sha1-checked,radio/modemfirmware.img,0f53162069dd7736eceb97d083d17000b5606824)
+$(call add-radio-file-sha1-checked,radio/multiimgqti.img,85c6e03d0dd54630f5017c9c83517fbe19ad6af1)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,af148da490135028d86d4f90f30cff362697f7bc)
+$(call add-radio-file-sha1-checked,radio/shrm.img,f08bcd48e85b02574debd16bd43e0cd65ec55bc0)
+$(call add-radio-file-sha1-checked,radio/tz.img,b9ffd17379e0820fe5593fe804307a36d8235dd3)
+$(call add-radio-file-sha1-checked,radio/uefi.img,f38afc30e45583954cb73604753a3a5586cdd3db)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,673f33398475762d0c8a4126e38fd43385687fe7)
+$(call add-radio-file-sha1-checked,radio/xbl.img,ef983156eba8b400ad7b787208e2b14f18d08852)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,b92f99c33c25c31c17f69fab1e8fd81dffe0d70b)
+$(call add-radio-file-sha1-checked,radio/xbl_ramdump.img,518a667ae8d3aa59f1d54ae36ee017d862cb1c44)
+
+endif
--- BoardConfigVendor.mk
+++ BoardConfigVendor.mk
@@ -1,3 +1,28 @@
 #
 # Automatically generated file. DO NOT MODIFY
 #
+
+AB_OTA_PARTITIONS += \
+    abl \
+    aop \
+    aop_config \
+    bluetooth \
+    cpucp \
+    cpucp_dtb \
+    devcfg \
+    dsp \
+    featenabler \
+    hyp \
+    imagefv \
+    keymaster \
+    modem \
+    modemfirmware \
+    multiimgqti \
+    qupfw \
+    shrm \
+    tz \
+    uefi \
+    uefisecapp \
+    xbl \
+    xbl_config \
+    xbl_ramdump
